6b315ce3dac75a8b7584e32e1d75b97495df2713
torinunna <torinunna9588@gmail.com>
2022-03-16 16:05:49 +0900

    challenge - link up the height slider and the weight slider with IBActions print out of the value of each slider

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
 }
 
