2f562b9c045264fc766d0c37a74c45357959a5de
torinunna <torinunna9588@gmail.com>
2022-03-19 14:27:44 +0900

    create a CalculatorBrain.swift and a struct called CalculatorBrain

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by 권유진 on 2022/03/19.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+}
