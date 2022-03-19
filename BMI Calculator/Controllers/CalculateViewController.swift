5a3558e038af722a9fbe625e29f23c09eefbca17
torinunna <torinunna9588@gmail.com>
2022-03-19 14:44:40 +0900

    challenge - fix the errors without writing any code inside CalculateViewController

--- BMI Calculator/Controllers/CalculateViewController.swift
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class CalculateViewController: UIViewController {
     
-    var bmiValue = "0.0"
+    var calculatorBrain = CalculatorBrain()
+    
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
@@ -36,16 +37,15 @@ class CalculateViewController: UIViewController {
         let height = heightSlider.value
         let weight = weightSlider.value
         
-        let bmi = weight / pow(height, 2)
-        bmiValue = String(format: "%.1f", bmi)
-    
+        calculatorBrain.calculateBMI(height: height, weight: weight)
+
         self.performSegue(withIdentifier: "goToResult", sender: self)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToResult" {
             let destinationVC = segue.destination as! ResultViewController
-            destinationVC.bmiValue = bmiValue
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
         }
     }
     
--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -7,7 +7,18 @@
 //
 
 import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
+    var bmi: Float = 0.0
+    
+    func getBMIValue() -> String{
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi)
+        return bmiTo1DecimalPlace
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float) {
+        bmi = weight / (height * height)
+    }
 }
