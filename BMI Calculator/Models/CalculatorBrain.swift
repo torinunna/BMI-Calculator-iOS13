3617477ffc89d0bebcc29d09d796165cf7602e44
torinunna <torinunna9588@gmail.com>
2022-03-19 17:18:46 +0900

    add advice message and background color instead of bmi classification challenge - Complete the code to change the advice message and background colour based on bmi value, when the app runs

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -18,16 +18,25 @@ struct CalculatorBrain {
         return bmiTo1DecimalPlace
     }
     
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
+    }
+
+    
     mutating func calculateBMI(height: Float, weight: Float) {
         let bmiValue = weight / (height * height)
         
         if bmiValue < 18.5 {
-            print("Underweight")
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.blue  )
         } else if bmiValue < 24.9 {
-            print("Normal")
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!!", color: UIColor.green)
         } else {
-            print("Overweight")
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor.red)
         }
-//        bmi = BMI(value: bmiValue, advice: <#T##String#>, color: <#T##UIColor#>)
+//
     }
 }
